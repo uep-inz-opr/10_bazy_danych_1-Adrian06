d1c8e20f8e3617061be3ee54e831c4131ac4d6a3
Adrian06 <31842146+Adrian06@users.noreply.github.com>
2022-05-19 14:53:41 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,39 @@
+import csv, sqlite3
+
+class ReportGenerator:
+  def __init__(self,connection, filename):
+    self.connection = connection
+    self.filename = filename
+
+  def generate_report(self):
+  	cursor = self.connection.cursor()
+  	cursor.execute("Select sum(duration) from polaczenia;")
+  	result = cursor.fetchall()
+  	self.report_text = int(result[0][0])
+
+  def get_report(self):	
+    return self.report_text
+
 
 if __name__ == "__main__":
-    pass
+	sqlite_con = sqlite3.connect("polaczenia.db")
+	cur = sqlite_con.cursor()
+
+	cur.execute('''CREATE TABLE polaczenia (from_subscriber data_type INTEGER, 
+	                  to_subscriber data_type INTEGER, 
+	                  datetime data_type timestamp, 
+	                  duration data_type INTEGER , 
+	                  celltower data_type INTEGER);''')
+
+	file = input()
+
+	with open(file,'r') as fin:
+	    reader = csv.reader(fin, delimiter = ";")
+	    next(reader, None)
+	    rows = [x for x in reader]
+	    cur.executemany("INSERT INTO polaczenia (from_subscriber, to_subscriber, datetime, duration , celltower) VALUES (?, ?, ?, ?, ?);", rows)
+	    sqlite_con.commit()
+
+	rp = ReportGenerator(sqlite_con, file)
+	rp.generate_report()
+	print(rp.get_report())
